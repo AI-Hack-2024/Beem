fad822e359e2bfc04e323490f51dad9b7f07702e
wanning <z5371162@unsw.edu.au>
2024-07-02 19:02:05 +1000

    emoji locations showing from the json

---
+++ frontend/src/lib/locations.js
@@ -0,0 +1,22 @@
+const Locations = [
+  {
+    name: 'Opera Bar',
+    location: [-33.85833, 151.21385],
+    url: 'https://g.co/kgs/s1UKabU',
+    icon: 'https://emojik.vercel.app/s/1f979_1f617?size=128',
+  },
+  {
+    name: 'Maybe Sammy',
+    location: [-33.8664, 151.2094],
+    url: 'https://g.co/kgs/7q3Z6m',
+    icon: 'https://emojik.vercel.app/s/1f37a?size=128',
+  },
+  {
+    name: 'The Baxter Inn',
+    location: [-33.8725, 151.2069],
+    url: 'https://g.co/kgs/8Y5v7L',
+    icon: 'https://emojik.vercel.app/s/1f37a?size=128',
+  },
+];
+
+export default Locations;
--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import LeafletControlGeocoder from '../components/LeafletControlGeocoder';
+import LocationCard from '../components/LocationCard';
+import * as HoverCard from '@radix-ui/react-hover-card';
+import Locations from '../lib/locations';
 
 const Map = () => {
-  const position = [-33.8657, 151.2159];
-  const operaBar = [-33.85833, 151.21385];
+  const SydneyPosition = [-33.8657, 151.2159];
+
+  const createCustomIcon = (iconUrl) => {
+    return new L.Icon({
+      iconUrl,
+      iconSize: [50, 50],
+      iconAnchor: [15, 30],
+      popupAnchor: [0, -30],
+    });
+  };
+
+  console.log(Locations);
 
   return (
     <>
       <h1>THIS IS WORKING</h1>
       <MapContainer
-        center={position}
+        center={SydneyPosition}
         zoom={15}
         scrollWheelZoom={false}
         style={{
@@ -22,9 +35,14 @@ const Map = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
         />
-        <Marker position={operaBar}>
-          <Popup>Opera Bar</Popup>
-        </Marker>
+        {Locations.map((location) => (
+          <Marker
+            position={location.location}
+            icon={createCustomIcon(location.icon)}
+          >
+            <Popup>{location.name}</Popup>
+          </Marker>
+        ))}
         <LeafletControlGeocoder />
       </MapContainer>
     </>
