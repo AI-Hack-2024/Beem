a7be7e3e0fb5b46827ac1a31a1f7f973a10b2e68
wanning <z5371162@unsw.edu.au>
2024-07-02 16:24:26 +1000

    sample map rendering in JS mode

--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import React from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 const Map = () => {
-    return (
-        <MapContainer
-            className="full-height-map"
-            center={[38, 139.69222]}
-            zoom={6}
-            minZoom={3}
-            maxZoom={19}
-            maxBounds={[[-85.06, -180], [85.06, 180]]}
-            scrollWheelZoom={true}>
-            <TileLayer
-                attribution='&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
-                url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-            />
-            TODO: Add markers
-        </MapContainer>
-    )
-}
+  const position = [51.505, -0.09];
+  return (
+    <>
+      <h1>THIS IS WORKING</h1>
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{
+          height: '700px',
+        }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </>
+  );
+};
 
-export default Map
+export default Map;
