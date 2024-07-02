d2c8a87a13b6cc31fe7e49fb9b8b91ccb04ba7b4
wanning <z5371162@unsw.edu.au>
2024-07-02 17:35:49 +1000

    map search functionality

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import 'leaflet/dist/leaflet.css';
+import 'leaflet-control-geocoder/dist/Control.Geocoder.css';
+import 'leaflet-control-geocoder/dist/Control.Geocoder.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import LeafletControlGeocoder from '../components/LeafletControlGeocoder';
 
 const Map = () => {
-  const position = [51.505, -0.09];
+  const position = [-33.8657, 151.2159];
+  const operaBar = [-33.85833, 151.21385];
+
   return (
     <>
       <h1>THIS IS WORKING</h1>
       <MapContainer
         center={position}
-        zoom={13}
+        zoom={15}
         scrollWheelZoom={false}
         style={{
           height: '700px',
         }}
+        className='fixed inset-x-0 bottom-0'
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
         />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
+        <Marker position={operaBar}>
+          <Popup>Opera Bar</Popup>
         </Marker>
+        <LeafletControlGeocoder />
       </MapContainer>
     </>
   );
