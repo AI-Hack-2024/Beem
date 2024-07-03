62ba918e6f84625ffbb9ef2eda46611063e60662
wanning <z5371162@unsw.edu.au>
2024-07-03 15:20:54 +1000

    location card

--- frontend/src/components/LocationCard.jsx
+++ frontend/src/components/LocationCard.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 
-const LocationCard = () => {
+const LocationCard = ({ name, imageUrl1, imageUrl2 }) => {
   return (
-    <div className='card bg-base-100 w-96 shadow-xl z-40 fixed bottom-16'>
+    <div className='card bg-base-100 w-96 shadow-xl z-40 fixed bottom-16 z-40'>
       <div className='flex'>
         <figure className='px-2 pt-2'>
           <img
-            src='https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg'
-            alt='Shoes'
+            src={imageUrl1} // First image URL
+            alt='Location Image 1'
             className='rounded-xl'
           />
         </figure>
         <figure className='px-2 pt-2'>
           <img
-            src='https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg'
-            alt='Shoes'
+            src={imageUrl2} // Second image URL
+            alt='Location Image 2'
             className='rounded-xl'
           />
         </figure>
       </div>
       <div className=''>
         <div className='flex justify-between py-2'>
-          <h2 className='card-title'>Location Name</h2>
+          <h2 className='card-title'>{name}</h2>
           <button className='btn btn-primary btn-sm'>View in AR</button>
         </div>
         <p className='text-left'>
