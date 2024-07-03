ea93301f695a2670559cf9f0e7df5874272ebfe4
wanning <z5371162@unsw.edu.au>
2024-07-03 15:11:55 +1000

    align banner

--- frontend/src/components/TryBanner.jsx
+++ frontend/src/components/TryBanner.jsx
@@ -4,7 +4,7 @@ import sparkleImage from '../assets/sparkle.png';
 const TryBanner = () => {
   // TODO: Move this to the correct position according to the figma
   return (
-    <div className='skeleton inline-flex items-center p-2'>
+    <div className='skeleton inline-flex items-center text-center p-2 z-40'>
       <img src={sparkleImage} alt='Sparkle' className='h-5 w-5 mr-1' />
       <p className='ml-1'>Try Magic Suggestions</p>
     </div>
