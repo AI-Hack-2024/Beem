9ed2c61e697fcf5412972f85f0b797a244fd3e3f
gracexkan <80564637+gracexkan@users.noreply.github.com>
2024-07-04 15:02:33 +1000

    frontend(fix): fixed icon sizing

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -1,7 +1,7 @@
 import L from 'leaflet';
 
 export default L.icon({
-  iconSize: [25, 41],
+  iconSize: [35, 35],
   iconAnchor: [10, 41],
   popupAnchor: [2, -40],
   iconUrl: 'https://emojik.vercel.app/s/1f600_1f30d?size=128',
