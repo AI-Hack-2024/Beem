354d539c6379cdeac99a398643887170564add17
wanning <z5371162@unsw.edu.au>
2024-07-04 13:34:25 +1000

    demo polish emoji search

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -4,6 +4,6 @@ export default L.icon({
   iconSize: [25, 41],
   iconAnchor: [10, 41],
   popupAnchor: [2, -40],
-  iconUrl: 'https://emojik.vercel.app/s/1f920_1f438?size=128',
+  iconUrl: 'https://emojik.vercel.app/s/1f600_1f30d?size=128',
   shadowUrl: 'https://unpkg.com/leaflet@1.6/dist/images/marker-shadow.png',
 });
