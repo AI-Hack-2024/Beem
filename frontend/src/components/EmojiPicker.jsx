6cde27c8616526e85ba32114530f7f56b4cab8d1
gracexkan <80564637+gracexkan@users.noreply.github.com>
2024-07-04 12:59:37 +1000

    fix: attempting to resolve deployment issues

--- frontend/src/components/EmojiPicker.jsx
+++ frontend/src/components/EmojiPicker.jsx
@@ -1,23 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import data from '@emoji-mart/data/sets/13.1/native.json';
 import { Picker } from 'emoji-kitchen-mart';
 
 function EmojiPicker() {
+  const pickerRef = useRef(null);
+
   useEffect(() => {
     const picker = new Picker({
-      parent: document.querySelector('#picker'),
+      parent: pickerRef.current,
       data: data,
       noCountryFlags: true,
     });
 
     return () => {
-      document.querySelector('#picker').innerHTML = '';
+      if (pickerRef.current) {
+        pickerRef.current.innerHTML = '';
+      }
     };
   }, []);
 
   return (
     <div>
-      <div id='picker'></div>
+      <div id='picker' ref={pickerRef}></div>
     </div>
   );
 }
